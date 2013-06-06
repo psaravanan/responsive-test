a47bcc454850c95129afc21ae03255dee3c3f151
shriram <shriram@shriram-desktop.(none)>
2013-06-06 10:33:45 +0530

    first and before bundle zurb install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebook::Application.config.secret_token = 'a8ab1e0e55ecfeb527db46cd521ef65a81f434816cce7a2d3892d5e37ec033df61474dc5ac1abcd44d03abee4d63f13c722340e3a2aa6bc0810d63e21f069157'
